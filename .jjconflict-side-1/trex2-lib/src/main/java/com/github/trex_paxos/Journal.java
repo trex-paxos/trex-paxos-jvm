5a42fd4fa229dc9b1cacd225689eb298b557e88c
simbo1905 <simon@simonmassey.org>
2024-03-29 11:32:30 +0000

    not liking jj just now

--- .jjconflict-side-1/trex2-lib/src/main/java/com/github/trex_paxos/Journal.java
+++
@@ -1,7 +0,0 @@
-package com.github.trex_paxos;
-
-public interface Journal {
-    void journalProgress(Progress progress);
-
-    void journalAccept(Accept accept);
-}
--- .jjconflict-side-1/trex2-lib/src/main/java/com/github/trex_paxos/PaxosRole.java
+++
@@ -1,7 +0,0 @@
-package com.github.trex_paxos;
-
-public enum PaxosRole {
-    FOLLOWER,
-    RECOVERER,
-    LEADER
-}
