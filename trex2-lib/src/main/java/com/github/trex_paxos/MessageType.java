4cf5d8fee133589d68feb904562f7494cd05f353
Simon Massey <simon@simonmassey.org>
2024-04-14 20:19:10 +0100

    ordinal to class map

--- trex2-lib/src/main/java/com/github/trex_paxos/MessageType.java
+++ trex2-lib/src/main/java/com/github/trex_paxos/MessageType.java
@@ -36,6 +36,16 @@ public enum MessageType {
     return ORDINAL_TO_TYPE_MAP.get(id);
   }
 
+  public static final Map<Byte, Class<? extends TrexMessage>> ORDINAL_TO_CLASS_MAP = Map.of(
+      (byte) 0, Prepare.class,
+      (byte) 1, PrepareResponse.class,
+      (byte) 2, Accept.class,
+      (byte) 3, AcceptResponse.class,
+      (byte) 4, Commit.class,
+      (byte) 5, Catchup.class,
+      (byte) 6, CatchupResponse.class
+  );
+
   public static MessageType fromPaxosMessage(TrexMessage trexMessage) {
     return switch (trexMessage) {
       case Prepare _ -> Prepare;
