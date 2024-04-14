984a8a35d8deeff4162e0bef342138641121528b
Simon Massey <simon@simonmassey.org>
2024-04-14 20:09:59 +0100

    make MessageType public

--- trex2-lib/src/main/java/com/github/trex_paxos/TrexMessage.java
+++ trex2-lib/src/main/java/com/github/trex_paxos/TrexMessage.java
@@ -2,55 +2,8 @@ package com.github.trex_paxos;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public sealed interface TrexMessage permits Accept, AcceptResponse, Catchup, CatchupResponse, Commit, Prepare, PrepareResponse {
   void writeTo(DataOutputStream dos) throws IOException;
 }
 
-enum MessageType {
-  Prepare(1),
-  PrepareResponse(2),
-
-  Accept(3),
-  AcceptResponse(4),
-  Commit(5),
-  Catchup(6),
-  CatchupResponse(7);
-
-  private final byte id;
-
-  MessageType(int id) {
-    this.id = (byte) id;
-  }
-
-  public Byte id() {
-    return id;
-  }
-
-  public static final Map<Byte, MessageType> ORDINAL_TO_TYPE_MAP;
-
-  static {
-    ORDINAL_TO_TYPE_MAP = Arrays.stream(values())
-      .collect(Collectors.toMap(MessageType::id, Function.identity()));
-  }
-
-  public static MessageType fromId(byte id) {
-    return ORDINAL_TO_TYPE_MAP.get(id);
-  }
-
-  public static MessageType fromPaxosMessage(TrexMessage trexMessage) {
-    return switch (trexMessage) {
-      case Prepare _ -> Prepare;
-      case PrepareResponse _ -> PrepareResponse;
-      case Accept _ -> Accept;
-      case AcceptResponse _ -> AcceptResponse;
-      case Commit _ -> Commit;
-      case Catchup _ -> Catchup;
-      case CatchupResponse _ -> CatchupResponse;
-    };
-  }
-}
