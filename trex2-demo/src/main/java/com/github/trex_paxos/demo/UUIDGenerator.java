e6f32977cd042a138a7fda6dac2a9ea22b9240ae
Simon Massey <simon@simonmassey.org>
2024-10-29 23:06:11 +0000

    docs

--- trex2-demo/src/main/java/com/github/trex_paxos/demo/UUIDGenerator.java
+++ trex2-demo/src/main/java/com/github/trex_paxos/demo/UUIDGenerator.java
@@ -4,13 +4,14 @@ import java.security.SecureRandom;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
-/// The core java UUID generator class can can only create weak Type 3 md5 based and strong Type 4 random based
-/// UUID. Here we use the current time in milliseconds and a counter in the most significant bits and a secure random
+/// The core java UUID generator class can can only create weak Type 3 and strong Type 4 random UUIDs.
+/// Here we use the current time in milliseconds and a counter in the most significant bits with a secure random
 /// in the lowest bits. This gives us good time based ordering within a single JVM. Across multiple JVMs it will be
-/// biased towards the JVM with the most recent time and the highest counter. This is a good trade-off for our use case.
+/// biased towards the JVM with the most recent time and highest counter. We initialize the counter to the startup
+/// time which may favour the machine which was started last. This is a good trade-off for our use case.
 ///
-/// The RFC for time based UUIDs suggest that 10M UUIDs per second per node can be generated. Testing the Java Type 4
-/// UUID generation it gets about 0.6M. This class gets about 0.5M.
+/// The RFC for time based UUIDs suggest that 10M UUIDs per second can be generated. Testing the Java core Type 4
+/// UUID generation it gets about 0.6M per second. This class gets about 0.5M per second.
 public class UUIDGenerator {
   /// A trick from the core UUID class is to use holder class to defer initialization until needed.
   private static class LazyRandom {
@@ -18,6 +19,7 @@ public class UUIDGenerator {
   }
 
   private static class LazyCounter {
+    // This counter starts off biased towards the server which started last.
     private static final AtomicLong sequence = new AtomicLong(System.currentTimeMillis());
   }
 
