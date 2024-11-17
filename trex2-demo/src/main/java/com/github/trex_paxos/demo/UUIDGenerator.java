86e45727898653f017d79a5d2c84a0a9e0ac714d
Simon Massey <simon@simonmassey.org>
2024-11-17 17:20:52 +0000

    docs on uuid gen

--- trex2-demo/src/main/java/com/github/trex_paxos/demo/UUIDGenerator.java
+++ trex2-demo/src/main/java/com/github/trex_paxos/demo/UUIDGenerator.java
@@ -4,37 +4,40 @@ import java.security.SecureRandom;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
-/// The core java UUID generator class can can only create weak Type 3 and strong Type 4 random UUIDs.
-/// Here we use the current time in milliseconds and a counter in the most significant bits with a secure random
-/// in the lowest bits. This gives us good time based ordering within a single JVM. Across multiple JVMs it will be
-/// biased towards the JVM with the most recent time and highest counter. We initialize the counter to the startup
-/// time which may favour the machine which was started last. This is a good trade-off for our use case.
-///
-/// The RFC for time based UUIDs suggest that 10M UUIDs per second can be generated. Testing the Java core Type 4
-/// UUID generation it gets about 0.6M per second. This class gets about 0.5M per second.
+/// The core java UUID generator class can can only create weak Type 3 md5 UUIDs and strong Type 4 random UUIDs.
+/// While it can read Type 1 time based UUIDs it cannot create them. This class creates time ordered UUIDs that are
+/// not official UUIDs but work for our purposes.
+/// The Java UUID library lets us create a UUID two longs. In the most significant long we put the time in milliseconds
+/// ito the higher bits and an atomic counter in the lowest bits.
+/// This gives us good time based ordering within a single JVM.
+/// For the last significant bits we use a pure random long. That effectively makes all of our UUIDs unique across
+/// all JVMs.
+/// The RFC for time based UUIDs suggest that 10M UUIDs per second can be generated. On an M1 Mac the Java core Type 4
+/// pure random UUID generation gives me about 0.6M per second. This class gets about 0.5M per second.
 public class UUIDGenerator {
   /// A trick from the core UUID class is to use holder class to defer initialization until needed.
   private static class LazyRandom {
     static final SecureRandom RANDOM = new SecureRandom();
   }
 
-  private static class LazyCounter {
-    // This counter starts off biased towards the server which started last.
-    private static final AtomicLong sequence = new AtomicLong(System.currentTimeMillis());
-  }
+  private static final AtomicLong sequence = new AtomicLong();
 
   /// This takes the Unix/Java epoch time in milliseconds, bit shifts it left by 20 bits, and then masks in the least
-  /// significant 20 bits of the local counter.
-  public static long uniqueTimestamp() {
+  /// significant 20 bits of the local counter. That gives us a million unique values per millisecond.
+  static long epochTimeThenCounterMsb() {
     long currentMillis = System.currentTimeMillis();
     // Take the least significant 20 bits from our atomic sequence
-    long subMillis = LazyCounter.sequence.incrementAndGet() & 0xFFFFF;
-    return (currentMillis << 20) | subMillis;
+    long counter20bits = sequence.incrementAndGet() & 0xFFFFF;
+    return (currentMillis << 20) | counter20bits;
   }
 
+  /// There is no guarantee that the time+counter of the most significant long will be unique across JVMs.
+  /// In the lower 64 bits we use a random long. This makes it improbably to get any collisions across JVMs.
+  /// Within a given JVM we will have good time based ordering.
   public static UUID generateUUID() {
-    // Get the most significant bits
-    long msb = uniqueTimestamp();
+    // As the most significant bits use ms time then counter for sub-millisecond ordering.
+    long msb = epochTimeThenCounterMsb();
+    // As the least significant bits use a random long which will give us uniqueness across JVMs.
     long lsb = LazyRandom.RANDOM.nextLong();
     return new UUID(msb, lsb);
   }
