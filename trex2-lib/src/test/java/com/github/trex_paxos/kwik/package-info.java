d372c37a35968bcdd2df5a9a7957c049546b34cb
Simon Massey <simbo1905@60hertz.com>
2024-12-27 07:52:41 +0000

    remove Transport class

--- trex2-lib/src/test/java/com/github/trex_paxos/kwik/package-info.java
+++
@@ -1,5 +0,0 @@
-/// This package is under src/test/java package and is a test dependency of the project.
-/// This means we do not ship with it. You will have to include it yourself if you want to use it.
-///
-/// This package is `Kwik!` Transport implementation which is a QUIC transport implementation.
-package com.github.trex_paxos.kwik;
