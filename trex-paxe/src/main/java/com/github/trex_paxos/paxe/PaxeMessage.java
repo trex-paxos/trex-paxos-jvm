95b092c444bad997c3a0123ebac9c242005f5034
Simon Massey <simbo1905@60hertz.com>
2024-12-31 11:00:31 +0000

    PaxePacketTest

--- trex-paxe/src/main/java/com/github/trex_paxos/paxe/PaxeMessage.java
+++ trex-paxe/src/main/java/com/github/trex_paxos/paxe/PaxeMessage.java
@@ -1,11 +1,42 @@
 package com.github.trex_paxos.paxe;
+import java.util.Arrays;
+import java.util.Objects;
 
-public interface PaxeMessage {
-    Channel channel();
-    byte[] serialize();
-    NodeId to();
-    NodeId from();
-    static PaxeMessage deserialize(byte[] decrypted) {
-        throw new AssertionError("Unimplemented method 'deserialize'");
+public record PaxeMessage(
+    NodeId from,
+    NodeId to,
+    Channel channel,
+    byte[] payload
+) {
+    public PaxeMessage {
+        Objects.requireNonNull(from, "from cannot be null");
+        Objects.requireNonNull(to, "to cannot be null");
+        Objects.requireNonNull(channel, "channel cannot be null");
+        Objects.requireNonNull(payload, "payload cannot be null");
+    }
+
+    public byte[] serialize() {
+        return payload;
+    }
+    
+    public static PaxeMessage deserialize(NodeId from, NodeId to, Channel channel, byte[] payload) {
+        return new PaxeMessage(from, to, channel, payload);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PaxeMessage that)) return false;
+        return from.equals(that.from)
+                && to.equals(that.to)
+                && channel.equals(that.channel)
+                && Arrays.equals(payload, that.payload);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(from, to, channel);
+        result = 31 * result + Arrays.hashCode(payload);
+        return result;
     }
 }
