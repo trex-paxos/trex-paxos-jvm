81e2d17d5becc46477ee4dc9005f4f1ff899a4ca
Simon Massey <simbo1905@60hertz.com>
2025-01-02 13:47:14 +0000

    fix Short->short

--- trex-lib/src/main/java/com/github/trex_paxos/BallotNumber.java
+++ trex-lib/src/main/java/com/github/trex_paxos/BallotNumber.java
@@ -19,7 +19,7 @@ package com.github.trex_paxos;
 /// nodeIdentifier in the least significant fifth byte. This works as long as we make the nodeIdentifier unique within the cluster
 /// at any given configuration. It must also be unique across the overlaps of cluster membership reconfigurations. We can use Paxos itself to
 /// ensure this uniqueness.
-public record BallotNumber(int counter, Short nodeIdentifier) implements Comparable<BallotNumber> {
+public record BallotNumber(int counter, short nodeIdentifier) implements Comparable<BallotNumber> {
 
   public static final BallotNumber MIN = new BallotNumber(Integer.MIN_VALUE, Short.MIN_VALUE);
 
