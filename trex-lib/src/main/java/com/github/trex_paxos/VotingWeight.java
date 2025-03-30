20a8b0e04dbeb676a97dd441cc37797a8ad6e44d
Simon Massey <simbo1905@60hertz.com>
2025-03-30 22:46:53 +0100

    Update VotingWeight.java

--- trex-lib/src/main/java/com/github/trex_paxos/VotingWeight.java
+++ trex-lib/src/main/java/com/github/trex_paxos/VotingWeight.java
@@ -1,5 +1,8 @@
 package com.github.trex_paxos;
 
+/// We can optionally use voting weights to evaluate quoums.
+/// FPaxos (Flexible Paxos) and UPaxos (Unbounded Paxos) use 
+/// voting weights. 
 public record VotingWeight(NodeId nodeId, int weight) {
     public VotingWeight {
         if (weight < 0) throw new IllegalArgumentException("Voting weight must be non-negative");
